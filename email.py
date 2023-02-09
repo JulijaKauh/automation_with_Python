32d4dc62042fcf44b6694273cc2a9dff931d3070
JulijaKauh <122730979+JulijaKauh@users.noreply.github.com>
2023-02-09 20:08:33 +0100

    Login + email

--- email.py
+++ email.py
@@ -2,3 +2,8 @@ def login(name, surname):
     username = "{}{}".format(name[0], surname).lower()
     email = username + "@" + "domain.com"
     return "{} {} your email is: {}.".format(name, surname, email)
+
+print(login("Sam", "Smitth"))
+print(login("Katty", "Walter"))
+print(login("Peter", "James"))
+print(login("Dane", "Depp"))
