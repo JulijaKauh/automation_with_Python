558bbf7e7e43406ef3ca15b25b8552168f53892c
JulijaKauh <122730979+JulijaKauh@users.noreply.github.com>
2023-02-26 20:10:12 +0100

    Update email.py

--- email.py
+++ email.py
@@ -10,3 +10,15 @@ print(login("Katty", "Walter"))
 print(login("Peter", "James"))
 print(login("Dane", "Depp"))
 print(login("Mary", "Cloud"))
+
+#!/usr/bin/env python3
+def login():
+    name = input("Name: ")
+    surname = input("Surname: ")
+    username = "{}{}".format(name[0], surname).lower()
+    email = username + "@" + "domain.com"
+    with open("user_email.txt", "a") as file:
+        file.write("User: " + surname + " " + name + ". Login: " + username + ", email: " + email + "\n")
+    print("Hi {} {}, your username is {} and your email is  {}""".format(name, surname, username, email))
+
+login()
